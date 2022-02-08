40582539b4e42f50cf85e1953aa50b5395123023
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-08 15:33:10 -0700

    All opration is working

---
+++ calc.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question('What would you like to calculate?', function (input) {
+  const tokens = input.split(' ');
+
+  mathSymbol = tokens[0];
+  num1 = Number(tokens[1]);
+  num2 = Number(tokens[2]);
+
+  if (mathSymbol === 'x') {
+    console.log(`Answer: ${num1 * num2}`);
+  } else if (mathSymbol === '/') {
+    console.log(`Answer: ${num1 / num2}`);
+  } else if (mathSymbol === '-') {
+    console.log(`Answer: ${num1 - num2}`);
+  } else if (mathSymbol === '+') {
+    console.log(`Answer: ${num1 + num2}`);
+  } else {
+    console.log("Sorry don't know that operator");
+  }
+  reader.close();
+});
